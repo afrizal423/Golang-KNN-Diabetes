96b9bf8f35fa5b3a0f353ff89e718ee29f058b55
afrizal423 <afrizal2499@gmail.com>
2020-11-23 11:56:48 +0700

    step 2 - cleaning data
    penjelasan:
    menghilangkan value 0 pada kolom
    Glucose,BloodPressure,SkinThickness,BMI,Insulin

---
+++ core/preprocessing_data.go
@@ -0,0 +1,125 @@
+package core
+
+import (
+	"fp_datmin_03_19_92/core/structs"
+)
+
+type mean struct {
+	Kolom string
+	Hasil float64
+}
+
+func Hilangkan_0(data []structs.DiabetesRecord, bantu []structs.DiabetesRecord) []structs.DiabetesRecord {
+	// var kolom = [...]string{"Insulin"}
+	// fmt.Println("jumlah data sebelum", len(data))
+	// fmt.Println("jumlah data sebelum", data[0].Insulin)
+	hasil := func(data []structs.DiabetesRecord, kolom string) float64 {
+		// var data []structs.DiabetesRecord
+		// var hitung []mean
+		// for i := range data {
+		// 	rulenya := mean{}
+		// 	if data[i].Insulin == 0 {
+		// 		rulenya.Kolom = "Insulin"
+		// 		rulenya.Hasil = 0
+		// 		hitung = append(hitung, rulenya)
+		// 	}
+		// }
+		// var hitung []mean
+
+		nilai_Glucose := 0.0
+		nilai_BloodPressure := 0.0
+		nilai_SkinThickness := 0.0
+		nilai_BMI := 0.0
+		nilai_Insulin := 0.0
+
+		for i := range data {
+			// rulenya := mean{}
+			if float64(data[i].Glucose) != float64(0) {
+				nilai_Glucose += data[i].Glucose
+			}
+			if float64(data[i].BloodPressure) != float64(0) {
+				nilai_BloodPressure += data[i].BloodPressure
+			}
+			if float64(data[i].SkinThickness) != float64(0) {
+				nilai_SkinThickness += data[i].SkinThickness
+			}
+			if float64(data[i].BMI) != float64(0) {
+				nilai_BMI += data[i].BMI
+			}
+			if float64(data[i].Insulin) != float64(0) {
+				nilai_Insulin += data[i].Insulin
+			}
+
+		}
+
+		if kolom == "Glucose" {
+			return nilai_Glucose / float64(len(data))
+		}
+		if kolom == "BloodPressure" {
+			return nilai_BloodPressure / float64(len(data))
+		}
+		if kolom == "SkinThickness" {
+			return nilai_SkinThickness / float64(len(data))
+		}
+		if kolom == "BMI" {
+			return nilai_BMI / float64(len(data))
+		}
+		if kolom == "Insulin" {
+			return nilai_Insulin / float64(len(data))
+		}
+
+		return 0.0
+		// fmt.Println(nilai_Glucose / float64(len(data)))
+		// fmt.Println(len(data))
+	}
+	mean_Glucose := hasil(bantu, "Glucose")
+	mean_BloodPressure := hasil(bantu, "BloodPressure")
+	mean_SkinThickness := hasil(bantu, "SkinThickness")
+	mean_BMI := hasil(bantu, "BMI")
+	mean_Insulin := hasil(bantu, "Insulin")
+
+	for i := range data {
+		// me := mean{}
+		if float64(data[i].Glucose) == float64(0) {
+			// fmt.Println("a asdasdasd sd", data[i].Insulin)
+			// fmt.Println("Sebelum ", data[i].Glucose)
+			data[i].Glucose = mean_Glucose
+			// fmt.Println("sesudah ", data[i].Glucose)
+		}
+		if float64(data[i].BloodPressure) == float64(0) {
+			// fmt.Println("a asdasdasd sd", data[i].Insulin)
+			// count_mean(bantu, "BloodPressure")
+			// fmt.Println(count_mean(bantu, "BloodPressure"))
+			data[i].BloodPressure = mean_BloodPressure
+		}
+		if float64(data[i].SkinThickness) == float64(0) {
+			// fmt.Println("a asdasdasd sd", data[i].Insulin)
+			// count_mean(bantu, "SkinThickness")
+			// fmt.Println(count_mean(bantu, "SkinThickness"))
+			data[i].SkinThickness = mean_SkinThickness
+		}
+		if float64(data[i].BMI) == float64(0) {
+			// fmt.Println("a asdasdasd sd", data[i].Insulin)
+			// count_mean(bantu, "BMI")
+			data[i].BMI = mean_BMI
+			// fmt.Println(count_mean(bantu, "BMI"))
+		}
+		if float64(data[i].Insulin) == float64(0) {
+			// fmt.Println("a asdasdasd sd", data[i].Insulin)
+			// count_mean(bantu, "Insulin")
+			// fmt.Println("Sebelum ", data[i].Insulin)
+			data[i].Insulin = mean_Insulin
+			// fmt.Println("sesudah ", data[i].Insulin)
+			// fmt.Println(count_mean(	bantu, "Insulin"))
+		}
+	}
+	// fmt.Println("jumlah data sesudah", data[0].Insulin)
+	var dataset []structs.DiabetesRecord
+	for i := range data {
+		// memasukkan ke array dataset
+		dataset = append(dataset, data[i])
+
+	}
+	return dataset
+	// fmt.Println(hasil(bantu, "Insulin"))
+}
--- main.go
+++ main.go
@@ -9,10 +9,15 @@ func main() {
 	dataset := core.Buka_file("diabetes.csv")
 
 	// pisah data menjadi data train dan data test
-	persentase_pembagian_data := 0.4 // 40 persen data train
-	testset, trainset := core.Test_train_data(dataset, persentase_pembagian_data)
+	persentase_pembagian_data := 0.7 // 40 persen data train
+	// cleaning := core.Hilangkan_0(dataset)
 
+	dataclean := core.Hilangkan_0(dataset, dataset)
+	testset, trainset := core.Test_train_data(dataclean, persentase_pembagian_data)
+	// fmt.Println(core.Hilangkan_0(dataset))
+	fmt.Println("jumlah data clean", len(dataclean))
 	fmt.Println("jumlah data", len(dataset))
 	fmt.Println("test data", len(testset))
 	fmt.Println("train data", len(trainset))
+	// fmt.Println(trainset)
 }
